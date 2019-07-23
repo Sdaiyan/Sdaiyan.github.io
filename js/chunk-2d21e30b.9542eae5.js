5f2f977096a493f3b09c2ef2962e6c3fc65632ba
sdy <876240987@qq.com>
2019-07-23 15:18:30 +0800

    init

---
+++ js/chunk-2d21e30b.9542eae5.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21e30b"],{d504:function(t,n,e){"use strict";e.r(n);var l=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("ul",[e("li",[e("router-link",{attrs:{to:"/translate"}},[t._v("翻译")])],1)])},r=[],u={},a=u,s=e("2877"),c=Object(s["a"])(a,l,r,!1,null,null,null);n["default"]=c.exports}}]);
+//# sourceMappingURL=chunk-2d21e30b.9542eae5.js.map
